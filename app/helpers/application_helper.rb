739b6faaea1fa271bb989366deb8681d3fb0aa1e
Nik <nik.jovan1@gmail.com>
2022-11-22 09:44:42 +0100

    Refectoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,21 @@
 module ApplicationHelper
-    def display_notice
-        if notice.present
-        content_tag(:p, notice, class: "notice")  if notice.present?
-    end
-    def display_alert
-        content_tag(:p, alert, class: "alert") if alert.present?
-    end 
+  def display_notice
+    content_tag(:p, notice, class: "notice") if notice.present?
+  end
 
-    def display_navbar_options
-        <% if user_signed_in? %>
-            render 'dropdown_options'
+  def display_alert
+    content_tag(:p, alert, class: "alert") if alert.present?
+  end
+
+  def display_right_navbar_option
+    if user_signed_in?
+      render 'dropdown_options'
     else
-        render 'login_buttons'
+      render 'login_buttons'
     end
-    end
+  end
 
-    def display
-
+  def display_active_class(link_page)
+    "active" if current_page?(link_page)
+  end
 end
-   
-
