2b06813b47eacf24f2224572528d0dbec933d905
Nik <nik.jovan1@gmail.com>
2022-11-22 09:38:35 +0100

    change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+    def display_notice
+        if notice.present
+        content_tag(:p, notice, class: "notice")  if notice.present?
+    end
+    def display_alert
+        content_tag(:p, alert, class: "alert") if alert.present?
+    end 
+
+    def display_navbar_options
+        <% if user_signed_in? %>
+            render 'dropdown_options'
+    else
+        render 'login_buttons'
+    end
+    end
+
+    def display
+
 end
+   
+
