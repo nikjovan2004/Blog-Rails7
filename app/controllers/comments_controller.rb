a1e55f7e68bda5751b8f39a0769f87e0f3baa830
Nik <nik.jovan1@gmail.com>
2022-12-20 09:16:18 +0100

    komentarji_form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,23 @@
 class CommentsController < ApplicationController
-    def create
-        @post=Post.find(params[:post_id])
-        @comment =@post.comments.build(comment_params)
-        @comment.user = current_user
-        
-        
-        raise params.to_json
+  def create
+    @post=Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
 
-        respond_to do |format|
-            if @comment.save
-                format.html { redirect_to post_url(@comment.post), notice: "Comment was successfully created." }
-                format.json { render :show, status: :created, location: @comment }
-            else
-                format.html { render :new, status: :unprocessable_entity }
-                format.json { render json: @comment.errors, status: :unprocessable_entity }
-            end
-        end
-end
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to post_url(@post), notice: "Comment was successfully created." }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { redirect_to post_url(@post), alert: "There was a problem with your comment." }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
-private 
-
-def comment_params
-    params.require(:comment).permit(:body, :post_id)
-end
+  private
+    # Only allow a list of trusted parameters through.
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
