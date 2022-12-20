bca472d6579154f004d2a3912652a7d172f78617
Nik <nik.jovan1@gmail.com>
2022-12-20 09:14:42 +0100

    komentarji-forma

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+    def create
+        @post=Post.find(params[:post_id])
+        @comment =@post.comments.build(comment_params)
+        @comment.user = current_user
+        
+        
+        raise params.to_json
+
+        respond_to do |format|
+            if @comment.save
+                format.html { redirect_to post_url(@comment.post), notice: "Comment was successfully created." }
+                format.json { render :show, status: :created, location: @comment }
+            else
+                format.html { render :new, status: :unprocessable_entity }
+                format.json { render json: @comment.errors, status: :unprocessable_entity }
+            end
+        end
+end
+
+private 
+
+def comment_params
+    params.require(:comment).permit(:body, :post_id)
+end
 end
